c7efa7f03c57961bd9226cac8d7228352334a15a
Ganzp <ganzp@naver.com>
2020-01-28 23:45:30 +0900

    revision

--- src/api/Ride/RequestRide/RequestRide.resolvers.ts
+++ src/api/Ride/RequestRide/RequestRide.resolvers.ts
@@ -13,23 +13,38 @@ const resolvers: Resolvers = {
 			async (
 				_,
 				args: RequestRideMutationArgs,
-				{ req }
+				{ req, pubSub }
 			): Promise<RequestRideResponse> => {
 				const user: User = req.user
-				try {
-					const ride = await Ride.create({
-						...args,
-						passenger: user
-					}).save()
-					return {
-						ok: true,
-						error: null,
-						ride
+				if (!user.isRiding) {
+					try {
+						const ride = await Ride.create({
+							...args,
+							passenger: user
+						}).save()
+						pubSub.publish("rideRequest", {
+							NearbyRideSubscription: ride
+						})
+						user.isRiding = true // riding request 시작시 true로 변환
+						user.save()
+						return {
+							ok: true,
+							error: null,
+							ride
+						}
+					} catch (error) {
+						return {
+							ok: false,
+							error: error.message,
+							ride: null
+						}
 					}
-				} catch (error) {
+				} else {
+					user.isRiding = false // 테스트용 --> 나중에 삭제
+					user.save()
 					return {
 						ok: false,
-						error: error.message,
+						error: "You can't request two rides",
 						ride: null
 					}
 				}
